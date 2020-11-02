8ad1729dd1dcc330b81bb419e6137bc28fd3d94f
sara616b <sara616b@stud.kea.dk>
2020-11-02 12:12:31 +0100

    script added

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", hentData);
+
+
+//Henter json data og starter forsiden (async for at få loadet json før man går videre)
+async function hentData() {
+    console.log("hentData");
+
+    //json link: https://sarahfrederiksen.dk/kea/2_semester/tema9/ihs/wordpress/wp-json/wp/v2/
+    const link = "https://sarahfrederiksen.dk/kea/2_semester/tema9/ihs/wordpress/wp-json/wp/v2/"
+
+}
