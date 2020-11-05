aefb8801ff39f6676d3d209fd88d037645b139df
sara616b <sara616b@stud.kea.dk>
2020-11-05 11:47:21 +0100

    specialefag tilføjet og indhold hentet

---
+++ scripts/specialefag.js
@@ -0,0 +1,58 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", hentData);
+
+let template = document.querySelector("template");
+let fag;
+let container = document.querySelector("#content");
+
+
+//Henter json data og starter højskole-siden (async for at få loadet json før man går videre)
+async function hentData() {
+    console.log("hentData");
+
+    //json link: https://sarahfrederiksen.dk/kea/2_semester/tema9/ihs/wordpress/wp-json/wp/v2/
+    const link = "https://sarahfrederiksen.dk/kea/2_semester/tema9/ihs/wordpress/wp-json/wp/v2/faggruppe/87"
+    const respons = await fetch(link);
+    const json = await respons.json();
+
+    //vis splash
+    vis(json);
+
+    //filtrer fagene
+    filterContent();
+}
+
+//Indsætter data fra wordpress på rette pladser på forsiden
+function vis(data) {
+    console.log("vis");
+
+    //TODO - ret billeder og tekstfelter i wordpress og definer dem her
+    document.querySelector("#splash").style.backgroundImage = "url(" + data.splashbillede.guid + ")";
+    document.querySelector("#title").innerHTML = data.title.rendered;
+
+}
+
+//filtrer fagene efter faggruppe og viser dem
+async function filterContent() {
+
+    //json link: https://sarahfrederiksen.dk/kea/2_semester/tema9/ihs/wordpress/wp-json/wp/v2/
+    const faglink = "https://sarahfrederiksen.dk/kea/2_semester/tema9/ihs/wordpress/wp-json/wp/v2/fag?per_page=100"
+    const responsfag = await fetch(faglink);
+    const jsonfag = await responsfag.json();
+    let filtrerede;
+    console.log("Det virker!!!");
+
+    console.log(jsonfag);
+
+    //filtrer efter kategori og vis
+    jsonfag.forEach((fag) => {
+        if (fag.kategori == "Specialefag") {
+            const klon = template.cloneNode(true).content;
+            klon.querySelector("h2").textContent = fag.title.rendered;
+            container.appendChild(klon);
+            console.log("appendChild");
+        }
+    })
+}
+
